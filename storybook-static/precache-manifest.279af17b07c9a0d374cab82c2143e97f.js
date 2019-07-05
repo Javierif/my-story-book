9a4436715d3d40ccef21fc9888d8b2aea1348bec
Javier Iniesta Fernández <jiniesta@encamina.com>
2019-07-05 14:18:15 +0200

    my-story-book UI

---
+++ storybook-static/precache-manifest.279af17b07c9a0d374cab82c2143e97f.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5a291cce9d38b50305295f07c89f34",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b5aeec86d35a9b92a17a.bundle.js"
+  },
+  {
+    "url": "runtime~main.b5aeec86d35a9b92a17a.bundle.js"
+  },
+  {
+    "url": "vendors~main.b5aeec86d35a9b92a17a.bundle.js"
+  }
+]);
